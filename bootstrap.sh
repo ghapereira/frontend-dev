5e759ca06b6aa0525a133946c28e444e64d1c86b
Gustavo Pereira <ghapereira@gmail.com>
2017-02-11 11:46:07 -0200

    Using vim-config as submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,6 @@
 
 apt-get update
 
-# Install vim
-#------------
-apt-get install -y vim
-
 # Apache
 apt-get install -y apache2
 
@@ -25,46 +21,23 @@ EOF
 )
 echo "${VHOST}" > /etc/apache2/sites-enabled/000-default
 
-# Enable mod_rewrite
+# Unix utils
+
 a2enmod rewrite
-# Restart apache
 service apache2 restart
-
-# Install cURL
-# ------------
+apt-get install -y vim
 apt-get install -y curl
-
-# Install Git
-# -----------
 apt-get install -y git-core
-
-# Install Ag
-# ----------
 apt-get install -y silversearcher-ag
-
-# Install dos2unix
-# ----------------
 apt-get install -y dos2unix
-
-# Install htop
-# ------------
 apt-get install -y htop
 
-# Install Node js
-# ---------------
+# Web techs
 apt-get install -y python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
-
-# Upgrade NPM
-# -----------
 npm update -g npm
-
-# Install Bower
-# -------------
 npm install -g bower
-
-# Install Grunt
-# -------------
 npm install -g grunt-cli
+
